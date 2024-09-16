d78c758f60d447231e1962f2e8d750ffb23aebb2
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-09-16 14:48:55 -0700

    more in diseases

--- ClyCites/app/disease/page.tsx
+++ ClyCites/app/disease/page.tsx
@@ -1,15 +1,12 @@
 import Image from 'next/image'
 import Button from '../../components/Button'
-import AboutSection from "@/components/About/AboutSection";
-import AboutMission from '@/components/About/AboutMission';
-import AboutHome from '@/components/About/AboutHome';
-import Programs from '@/components/Programs';
+import Diseases from '@/components/Diseases'
 
 const DiseasePage = () => {
   return (
       // <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <>
-      <div className="max-container  -my-4 bg-gray-100">
+      <div className="max-container  -my-4 bg-gray-100 bg-[url('/images/agri.jpg')] bg-cover bg-center bg-no-repeat text-white p-8 rounded-lg">
               <div className="container mx-auto">
                   <div className="flex flex-wrap text-center justify-center al -mx-6 gap-2 lg:gap-6">
 
@@ -20,7 +17,7 @@ const DiseasePage = () => {
                   <p className="text-center justify-center text-white sm:text-2xl lg:text-xl xl:text-4xl">Proactively protect crops</p>
               </div>
         </div>
-        <Programs/>
+        <Diseases/>
         {/* <AboutHome />
 <div className="w-full border-t border-black mb-10" />
 <AboutSection />
