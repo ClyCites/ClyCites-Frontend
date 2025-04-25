4434cd383e29921d7837862aaf5059fa21e96f82
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-04-25 11:02:04 -0700

    mor b bb n bb

--- ClyCites/components/theme-provider.tsx
+++ ClyCites/components/theme-provider.tsx
@@ -1,12 +1,12 @@
 // components/theme-provider.tsx
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps as NextThemeProviderProps } from "next-themes";
 import { ReactNode } from "react";
 
-interface ThemeProviderProps {
+interface ThemeProviderProps extends NextThemeProviderProps {
   children: ReactNode;
 }
 
-export function ThemeProvider({ children }: ThemeProviderProps) {
-  return <NextThemesProvider>{children}</NextThemesProvider>;
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
