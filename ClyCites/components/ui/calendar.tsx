a20d129ce10f8ee64f708f3ca98dcc8492e85b9b
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-04-25 15:11:20 -0700

    mo nn BB

--- ClyCites/components/ui/calendar.tsx
+++ ClyCites/components/ui/calendar.tsx
@@ -1,22 +1,6 @@
 import { DayPicker } from "react-day-picker"
-import type { DayPickerProps } from "react-day-picker" // import the prop types
+import type { DayPickerProps } from "react-day-picker"
 
 function CustomCalendar(props: DayPickerProps) {
-  return (
-    <DayPicker
-      components={{
-        Navigation: (navProps) => (
-          <div className="flex justify-between px-2">
-            <button {...navProps.previousButtonProps} className="p-1">
-              <ChevronLeft className="h-4 w-4" />
-            </button>
-            <button {...navProps.nextButtonProps} className="p-1">
-              <ChevronRight className="h-4 w-4" />
-            </button>
-          </div>
-        ),
-      }}
-      {...props}
-    />
-  )
+  return <DayPicker {...props} />
 }
