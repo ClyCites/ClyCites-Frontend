fffe10e1fbb93f1f4c8694911219c17919b3dfba
Daniel Ntege <danientege785@gmail.com>
2025-05-31 19:14:15 +0300

    `Removed top bar from dashboard layout component`

--- ClyCites-accounts/components/dashboard-layout.tsx
+++ ClyCites-accounts/components/dashboard-layout.tsx
@@ -138,7 +138,6 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
       </Sheet>
 
       <div className="lg:pl-72">
-        {/* Top bar */}
         <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
           <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
             <div className="flex flex-1"></div>
